21a3db98ad277316702dd8a4e1bb8a9b989813a0
ArkadiySamokhvalov <arkadii.samohvalov@bk.ru>
2024-09-25 13:28:32 +0300

    feat: добавлено сохранение списка задач в localStorage

--- src/store/useTodoListStore.ts
+++ src/store/useTodoListStore.ts
@@ -6,22 +6,30 @@ type ToDoItem = {
   completed: boolean;
 };
 
+function getLocalStoredTodoList(): ToDoItem[] {
+  const todoList = localStorage.getItem('todoList');
+  return todoList ? JSON.parse(todoList) : [];
+}
+
 export const useTodoListStore = defineStore('todoList', {
   state: () => ({
-    todoList: <ToDoItem[]>[],
+    todoList: getLocalStoredTodoList(),
     id: 0
   }),
   actions: {
     addTodo(task: string) {
       this.todoList.push({ task, id: this.id++, completed: false });
+      localStorage.setItem('todoList', JSON.stringify(this.todoList));
     },
     removeTodo(id: number) {
       this.todoList = this.todoList.filter((item) => item.id !== id);
+      localStorage.setItem('todoList', JSON.stringify(this.todoList));
     },
     changeTask(id: number, task: string) {
       const todo = this.todoList.find((item) => item.id === id);
       if (todo) {
         todo.task = task;
+        localStorage.setItem('todoList', JSON.stringify(this.todoList));
       }
     },
     toggleCompleted(id: number) {
